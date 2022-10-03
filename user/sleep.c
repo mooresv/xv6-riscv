5b8d5872f271fc56f65f70f8d375ef30d188deb1
achacon14 <achacon14@miners.utep.edu>
2022-10-03 20:19:39 +0000

    Lab2

---
+++ user/sleep.c
@@ -0,0 +1,20 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char **argv)
+{
+  int i;
+
+  if(argc < 2){
+    fprintf(2, "usage: kill pid...\n");
+    exit(1);
+  }
+  for(i=1; i<argc; i++)
+    kill(atoi(argv[i]));
+  exit(0);
+}
+
+// sleep is not correct -- you use the actually sleep value from xv6
+
